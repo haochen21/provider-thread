31f5fb007ee2b782dcdecddba0278c4b348b8a75
haochen21 <chenhao21@163.com>
2019-04-23 22:00:14 +0800

    Initial commit

---
+++ src/main/java/com/beta/providerthread/ProviderThreadApplication.java
@@ -0,0 +1,13 @@
+package com.beta.providerthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderThreadApplication.class, args);
+    }
+
+}
